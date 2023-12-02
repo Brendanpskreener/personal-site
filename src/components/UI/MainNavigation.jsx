71258f8e0b2be59351556eb272fbf4b12fc2363c
Brendan Schreiner <brendanpschreiner@gmail.com>
2023-12-01 19:25:24 -0800

    initalized react router

--- src/components/UI/MainNavigation.jsx
+++ src/components/UI/MainNavigation.jsx
@@ -1,15 +1,23 @@
+import { NavLink } from 'react-router-dom'
 import classes from './MainNavigation.module.css'
 
-export default function MainNavigation({ onClick }) {
+export default function MainNavigation() {
 
   return (
     <ul className={classes.list}>
-      <li><a onClick={() => onClick('home')}>Home</a></li>
+      <li><NavLink
+        to='/'
+        className={({ isActive }) => isActive ? classes.active : undefined}
+        end>Home</NavLink></li>
       <li>
         <a>My Projects</a>
         <ul className={classes.dropdown}>
-          <li><a onClick={() => onClick('barFinder')}>Bar Finder</a></li>
-          <li><a onClick={() => onClick('pomodoro')}>Pomodoro</a></li>
+          <li><NavLink
+            to='/barfinder'
+            className={({ isActive }) => isActive ? classes.active : undefined}>Bar Finder</NavLink></li>
+          <li><NavLink
+            to='pomodoro'
+            className={({ isActive }) => isActive ? classes.active : undefined}>Pomodoro</NavLink></li>
         </ul>
       </li>
     </ul>

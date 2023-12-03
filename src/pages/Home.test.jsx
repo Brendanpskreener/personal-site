28b3cbc2586d6ea39a90a0e9f3bab1a67092c753
Brendan Schreiner <brendanpschreiner@gmail.com>
2023-12-03 15:40:37 -0800

    yet more config/libraries

--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -5,6 +5,6 @@ import Home from './Home'
 describe("Home page test", () => {
   test("Should show Home page", () => {
     render(<Home />)
-    expect(screen.getByText(/Brendan Schreiner/i)).toBeDefined()
+    expect(screen.getByText(/Brendan Schreiner/i)).toBeInTheDocument()
   })
 })

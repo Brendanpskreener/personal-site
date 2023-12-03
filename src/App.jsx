5fc656e4a8ebeb79de5d56ee80b7339213091cef
Brendan Schreiner <brendanpschreiner@gmail.com>
2023-12-03 13:06:34 -0800

    moved router out of app function

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import Home from './pages/Home'
 import BarFinder from './components/BarFinder/BarFinder'
 import Pomodoro from './components/Pomodoro/Pomodoro'
 
-export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      errorElement: <Error />,
-      children: [
-        { path: '/', element: <Home /> },
-        { path: '/barfinder', element: <BarFinder /> },
-        { path: '/pomodoro', element: <Pomodoro /> },
-      ]
-    }
-  ])
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/barfinder', element: <BarFinder /> },
+      { path: '/pomodoro', element: <Pomodoro /> },
+    ]
+  }
+])
 
+export default function App() {
   return (
     <>
       <RouterProvider router={router} />

265190acf9b3b9c44132dc5b79fd80df52b63e85
Brendan Schreiner <brendanpschreiner@gmail.com>
2023-12-03 22:12:50 -0800

    Adding tests (#5)
    * installed and configured
    
    * additional config, baseline tests
    
    * yet more config/libraries
    
    * unit test for Barfinder

---
+++ src/pages/Home.test.jsx
@@ -0,0 +1,10 @@
+import { describe, expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Home from './Home'
+
+describe("Home page test", () => {
+  test("Should show Home page", () => {
+    render(<Home />)
+    expect(screen.getByText(/Brendan Schreiner/i)).toBeInTheDocument()
+  })
+})

6b94f49f555832f272c57eed46c3835a01cacb9d
Brendan Schreiner <brendanpschreiner@gmail.com>
2023-12-11 18:31:13 -0800

    Mobile nav (#11)
    * working version
    
    * added burger

--- src/components/UI/MainNavigation.jsx
+++ src/components/UI/MainNavigation.jsx
@@ -1,26 +1,39 @@
 import { NavLink } from 'react-router-dom'
 import classes from './MainNavigation.module.css'
+import { useState } from 'react'
 
 export default function MainNavigation() {
+  const [isOpen, setIsOpen] = useState(false)
+  function toggleMenu() {
+    setIsOpen((prevState) => !prevState)
+  }
 
   return (
-    <div className={classes.list}>
-      <NavLink
-        to='/'
-        className={({ isActive }) => isActive ? classes.active : undefined}
-        end>
-        Home
-      </NavLink>
-      <NavLink
-        to='/barfinder'
-        className={({ isActive }) => isActive ? classes.active : undefined}>
-        Bar Finder
-      </NavLink>
-      <NavLink
-        to='pomodoro'
-        className={({ isActive }) => isActive ? classes.active : undefined}>
-        Pomodoro
-      </NavLink>
-    </div>
+    <>
+      <div className={classes.trigger} onClick={toggleMenu}>
+        <span className={classes.burger}></span>
+        <span className={classes.burger}></span>
+        <span className={classes.burger}></span>
+      </div>
+      <div className={isOpen ? classes.open : classes.list}>
+        <NavLink
+          to='/'
+          className={({ isActive }) => isActive ? classes.active : undefined}
+          end>
+          Home
+        </NavLink>
+        <NavLink
+          to='/barfinder'
+          className={({ isActive }) => isActive ? classes.active : undefined}>
+          Bar Finder
+        </NavLink>
+        <NavLink
+          to='pomodoro'
+          className={({ isActive }) => isActive ? classes.active : undefined}>
+          Pomodoro
+        </NavLink>
+      </div>
+    </>
+
   )
 }

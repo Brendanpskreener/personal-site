ea74458349b6a9dbbc134f8750d7ab773096bd3f
Brendan Schreiner <brendanpschreiner@gmail.com>
2024-01-31 13:10:58 -0800

    blurhash, pagination, styling

--- src/components/Store/Product.jsx
+++ src/components/Store/Product.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react"
 import findImage from '../../interfaces/FindImage'
+import { BlurhashCanvas } from "react-blurhash";
 
 export default function Product({
   name,
-  description,
+  blurHash,
   msrp,
   productId,
   slogan
 }) {
   const [image, setImage] = useState([])
+  const [imageLoading, setImageLoading] = useState(true)
 
   async function getImage() {
     try {
@@ -17,6 +19,7 @@ export default function Product({
       const uintStrBuf = uintBuf.reduce((acc, bytes) => (`${acc}${String.fromCharCode(bytes)}`), '');
       const base64String = btoa(uintStrBuf);
       setImage(base64String)
+      setImageLoading(false)
     } catch (error) {
       console.warn(error)
     }
@@ -31,7 +34,7 @@ export default function Product({
 
   return (
     <article className="product">
-      <img src={`data:image/png;base64, ${image}`} alt={slogan} />
+      {imageLoading ? <BlurhashCanvas hash={blurHash} width='330' height='330' /> : <img src={`data:image/png;base64, ${image}`} alt={slogan} />}
       <div className="product-content">
         <h3>{name}</h3>
         <span className="product-price">
--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,33 +1,44 @@
 import { useEffect, useState } from "react";
 import Product from "./Product";
 import findProducts from "../../interfaces/FindProducts";
+import StorePagination from "./StorePagination";
 
 export default function Store() {
   const [productList, setProductList] = useState([])
+  const [pagination, setPagination] = useState({ from: 0, size: 1, total: 0 })
+  const [currentPageNumber, setCurrentPageNumber] = useState(1)
+  const [userPageSize, setUserPageSize] = useState(5)
+
 
   async function getStore() {
     try {
-      const store = await findProducts()
-      setProductList(store.results)
+      const { results, pagination } = await findProducts({ from: (currentPageNumber - 1) * userPageSize, size: userPageSize })
+      setProductList(results)
+      setPagination(pagination)
     } catch (error) {
       console.warn(error)
     }
   }
 
+  function onPageChange(page) {
+    setCurrentPageNumber(page)
+  }
+
   useEffect(() => {
     getStore()
-  }, [])
+  }, [currentPageNumber, userPageSize])
+
+  const totalPageCount = Math.ceil(pagination.total / pagination.size)
 
   return (
     <section className="shop">
       <h1 className="title">AI Shop</h1>
       <ul className="products">
         {productList.map((product, index) => (
-          <div key={product.productId}>
-            <Product {...product} />
-          </div>
+          <Product key={product.productId} {...product} />
         ))}
       </ul>
+      {totalPageCount > 1 && <StorePagination totalPageCount={totalPageCount} onPageChange={onPageChange} currentPage={currentPageNumber} />}
     </section>
   )
 }

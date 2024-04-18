ad854c49a54fe10976548461875021b81f7d4e4e
Brendan Schreiner <brendanpschreiner@gmail.com>
2024-04-17 21:41:23 -0700

    Buttonstyle (#18)
    * Fixed the favorites filter to be the same style as the buttons
    
    * adding loading spinners
    
    * Loading spinner between searches
    
    * no longer relevant

--- src/components/BarFinder/BarFinder.jsx
+++ src/components/BarFinder/BarFinder.jsx
@@ -2,21 +2,23 @@ import { useContext } from "react"
 import UserInput from './UserInput'
 import Pagination from './Pagination'
 import BarList from './BarList'
+import LoadingRing from '../UI/LoadingRing'
 import classes from './BarFinder.module.css'
 import { BarFinderContext } from "../../store/BarFinderContext"
 
 export default function BarFinder() {
-  const { loading } = useContext(BarFinderContext)
+  const { loading, searchLoading } = useContext(BarFinderContext)
+
 
   return (
     <>
       <h1 className={classes.title}>
         Bar Finder
       </h1>
-      {loading ? <div className={classes.loading}>Loading...</div> :
+      {loading ? <div className={classes.loading}><LoadingRing /></div> :
         <div className={classes.container}>
           <UserInput />
-          <BarList />
+          {searchLoading ? <div className={classes.loading}><LoadingRing /></div> : <BarList />}
           <Pagination />
         </div>}
     </>
--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -2,28 +2,33 @@ import { useContext } from "react";
 import Product from "./Product";
 import StorePagination from "./StorePagination";
 import { StoreContext } from "../../store/StoreContext";
+import LoadingRing from "../UI/LoadingRing";
 
 export default function Store() {
-  const { productList, filtered, changeFilter, totalPageCount, favoritesList } = useContext(StoreContext)
+  const { productList, filtered, changeFilter, totalPageCount, favoritesList, loading } = useContext(StoreContext)
 
   return (
     <section className="shop">
-      <div className="header">
+      {loading ? <h1 className="title"><LoadingRing /></h1> : <><div className="header">
         <h1>AI Shop</h1>
         <div>
-          <span className={filtered === 0 ? 'selected' : null} onClick={() => changeFilter(0)}>All Products</span>
-          <span className={filtered === 1 ? 'selected' : null} onClick={() => changeFilter(1)}>Favorites</span>
+          <p className={filtered === 0 ? 'selected' : null}>
+            <span onClick={() => changeFilter(0)}>All Products</span>
+          </p>
+          <p className={filtered === 1 ? 'selected' : null} >
+            <span onClick={() => changeFilter(1)}>Favorites</span>
+          </p>
         </div>
       </div>
-      <ul className="products">
-        {productList.length > 0 ? productList.map((product, index) => {
-          const faved = favoritesList.includes(product.productId) ? true : false
-          return (
-            <Product key={product.productId} {...product} faved={faved} />
-          )
-        }) : <h1 className="title">No Results</h1>}
-      </ul>
-      {totalPageCount > 1 && <StorePagination />}
+        <ul className="products">
+          {productList.length > 0 ? productList.map((product, index) => {
+            const faved = favoritesList.includes(product.productId) ? true : false
+            return (
+              <Product key={product.productId} {...product} faved={faved} />
+            )
+          }) : <h1 className="title">No Results</h1>}
+        </ul>
+        {totalPageCount > 1 && <StorePagination />}</>}
     </section>
   )
 }

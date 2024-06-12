ec5f0e2c5a6916c5188f730b2f650b6dfa15ce54
Brendan Schreiner <brendanpschreiner@gmail.com>
2024-06-11 17:22:22 -0700

    removed the repeated plain html

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import classes from './Home.module.css'
 import storeImg from '../assets/Store.png'
 import barFinderImg from '../assets/BarFinder.png'
-import ghImg from '../assets/github-mark-white.png'
+import Projects from '../components/Home/Projects'
 
 export default function Home() {
   return (
@@ -11,30 +11,18 @@ export default function Home() {
         <div className={classes["projects"]}>
           <h2>Projects</h2>
           <div className={classes["content"]}>
-            <div className={classes["card"]}>
-              <div className={classes["description"]}>
-                <header>
-                  <h3>AI Store</h3>
-                  <a href='https://github.com/Brendanpskreener/personal-site/tree/main/src/components/Store' target="_blank">
-                    <img src={ghImg}></img>
-                  </a>
-                </header>
-                <p> Developed my own REST API using DynamoDB, AWS Lambda, Node, and OpenSearch (Elastic Search) to create a store page populated with AI generated products.</p>
-              </div>
-              <img src={storeImg} />
-            </div>
-            <div className={classes["card"]}>
-              <div className={classes["description"]}>
-                <header>
-                  <h3>Bar Finder</h3>
-                  <a href='https://github.com/Brendanpskreener/personal-site/tree/main/src/components/BarFinder' target="_blank">
-                    <img src={ghImg}></img>
-                  </a>
-                </header>
-                <p> Developed a React app hitting OpenBrewery API with React Hooks, Redux/Context management, Paginated results, CSS animations & transitions, async await, two-way binding, conditional content, etc</p>
-              </div>
-              <img src={barFinderImg} />
-            </div>
+            <Projects
+              name='AI Store'
+              url='https://github.com/Brendanpskreener/personal-site/tree/main/src/components/Store'
+              img={storeImg}
+              description='Developed my own REST API using DynamoDB, AWS Lambda, Node, and OpenSearch (Elastic Search) to create a store page populated with AI generated products.'
+            />
+            <Projects
+              name='Bar Finder'
+              url='https://github.com/Brendanpskreener/personal-site/tree/main/src/components/BarFinder'
+              img={barFinderImg}
+              description='Developed a React app hitting OpenBrewery API with React Hooks, Redux/Context management, Paginated results, CSS animations & transitions, async await, two-way binding, conditional content, etc'
+            />
           </div>
         </div>
       </div>

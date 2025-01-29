eff8d1ba8d1708cf1358fe6afa6699a905443a23
Brendan Schreiner <brendanpschreiner@gmail.com>
2025-01-28 21:32:33 -0800

    attempting to mock router component in Home page

--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -1,10 +1,15 @@
 import { describe, expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import Home from './Home'
+import { MemoryRouter } from 'react-router-dom'
 
 describe("Home page test", () => {
   test("Should show Home page", () => {
-    render(<Home />)
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>
+    )
     expect(screen.getByText(/Brendan/i)).toBeInTheDocument()
   })
 })

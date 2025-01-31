1b2a540485070272863706d02df1e84698c76df6
Brendan Schreiner <brendanpschreiner@gmail.com>
2025-01-30 18:56:35 -0800

    Stylecleanup (#37)
    * created new GHlinks component
    
    * fixed nav menu occlusion

--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -1,9 +1,9 @@
 import { NavLink } from 'react-router-dom'
-import gitHubLogo from '../../assets/github-mark-white.png'
 import classes from './Projects.module.css'
+import GitHubLink from '../GitHubLinks/GitHubLink'
 
 export default function Projects(props) {
-  const { name, url, url2, img, description, futurePlans, nav } = props
+  const { name, githubLinks, img, description, futurePlans, nav } = props
   return (
     <div className={classes["card"]}>
       <div className={classes["description"]}>
@@ -13,17 +13,16 @@ export default function Projects(props) {
               {name}
             </NavLink>
           </h3>
-          <div>
-            <a target="_blank" href={url}>
-              <img src={gitHubLogo}></img>
-            </a>
-            {url2 && <a target="_blank" href={url2}>
-              <img src={gitHubLogo}></img>
-            </a>}
-          </div>
         </header>
         <p>{description}</p>
         {futurePlans && <p>{futurePlans}</p>}
+        {githubLinks.map((githubLink, index) => (
+          <GitHubLink
+            key={index}
+            caption={githubLink.name}
+            url={githubLink.url}
+          />
+        ))}
       </div>
       <NavLink to={nav}>
         <img src={img} className={classes.imagelink} />

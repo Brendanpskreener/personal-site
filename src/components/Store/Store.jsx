b66cc338249636deed11df806a6074b9d6eb9a90
Brendan Schreiner <brendanpschreiner@gmail.com>
2026-02-06 13:57:14 -0800

    Skeletonloader (#39)
    * install react skeleton loader
    
    * apply global theme
    
    * iterate tiled skeletons before product list resolves

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -2,14 +2,27 @@ import { useContext } from "react";
 import Product from "./Product";
 import StorePagination from "./StorePagination";
 import { StoreContext } from "../../store/StoreContext";
-import LoadingRing from "../UI/LoadingRing";
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 export default function Store() {
   const { productList, filtered, changeFilter, totalPageCount, favoritesList, loading } = useContext(StoreContext)
+  const placeholder = Array(10).fill(0)
+  let context
+  if (productList.length > 0) {
+    context = productList.map((product, index) => {
+      const faved = favoritesList.includes(product.productId)
+      return (
+        <Product key={product.productId} {...product} faved={faved} />
+      )
+    })
+  } else {
+    context = <h1 className="title">No Results</h1>
+  }
 
   return (
     <section className="shop">
-      {loading ? <h1 className="title"><LoadingRing /></h1> : <><div className="header">
+      {<><div className="header">
         <h1>AI Shop</h1>
         <div>
           <p className={filtered === 0 ? 'selected' : null}>
@@ -21,12 +34,11 @@ export default function Store() {
         </div>
       </div>
         <ul className="products">
-          {productList.length > 0 ? productList.map((product, index) => {
-            const faved = favoritesList.includes(product.productId) ? true : false
+          {loading ? placeholder.map((element, index) => {
             return (
-              <Product key={product.productId} {...product} faved={faved} />
+              <Skeleton height={445} />
             )
-          }) : <h1 className="title">No Results</h1>}
+          }) : context}
         </ul>
         {totalPageCount > 1 && <StorePagination />}</>}
     </section>

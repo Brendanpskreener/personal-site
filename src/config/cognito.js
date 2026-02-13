5e771fbd1a2996dbc1778e9a000c4072758ad13b
Brendan Schreiner <brendanpschreiner@gmail.com>
2026-02-12 20:33:12 -0800

    config file for DNRY

---
+++ src/config/cognito.js
@@ -0,0 +1,7 @@
+export const cognitoConfig = {
+  region: 'us-west-2',
+  userPoolId: 'us-west-2_BhkNp7Dkh',
+  clientId: '436o9029lqnf04p4hkajp43d86',
+  domain: 'https://us-west-2bhknp7dkh.auth.us-west-2.amazoncognito.com',
+  redirectUri: 'http://localhost:5173'
+}

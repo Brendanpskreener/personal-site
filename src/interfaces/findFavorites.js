33ea763cbbfb0fcff9697342eb65efda891ec01c
Brendan Schreiner <brendanpschreiner@gmail.com>
2026-02-12 23:01:26 -0800

    added token to these calls

--- src/interfaces/findFavorites.js
+++ src/interfaces/findFavorites.js
@@ -1,10 +1,14 @@
-import axios from "axios";
+import apiWithAuth from '../utils/apiWithAuth'
 
-export default async function findFavorites(userId) {
+export default async function findFavorites(userId, token) {
   const baseURL = new URL(`https://xkw4oz08el.execute-api.us-west-2.amazonaws.com/user/${userId}/favorite`)
+  const config = {
+    method: 'get',
+    url: baseURL
+  }
 
   try {
-    const response = await axios.get(baseURL)
+    const response = await apiWithAuth(config, token)
     return response.data
   } catch (error) {
     console.error(error)

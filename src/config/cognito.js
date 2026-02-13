74d4dbc64e1ee911356103d9536d49b900951189
Brendan Schreiner <brendanpschreiner@gmail.com>
2026-02-13 01:35:19 -0800

    detecting environment (#43)

--- src/config/cognito.js
+++ src/config/cognito.js
@@ -3,5 +3,5 @@ export const cognitoConfig = {
   userPoolId: 'us-west-2_BhkNp7Dkh',
   clientId: '436o9029lqnf04p4hkajp43d86',
   domain: 'https://us-west-2bhknp7dkh.auth.us-west-2.amazoncognito.com',
-  redirectUri: 'http://localhost:5173'
+  redirectUri: window.location.origin
 }
